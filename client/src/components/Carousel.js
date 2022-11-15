a6f48bbd2a94adecb20899ad613f89afff0a4103
Micaela Frank <micaelalfrank@gmail.com>
2022-11-15 14:10:03 -0800

    test2

--- client/src/components/Carousel.js
+++ client/src/components/Carousel.js
@@ -1,21 +1,5 @@
 import React from 'react'
-import Carousel from "react-bootstrap/Carousel";
 
-import one from "../../images/MenoushPics/1.JPG"
-import two from "../../images/MenoushPics/2.JPG";
-import three from "../../images/MenoushPics/3.JPG";
-import four from "../../images/MenoushPics/4.JPG";
-import five from "../../images/MenoushPics/5.JPG";
-import six from "../../images/MenoushPics/6.JPG";
-import seven from "../../images/MenoushPics/7.JPG";
-import eight from "../../images/MenoushPics/8.JPG";
-import nine from "../../images/MenoushPics/9.JPG";
-import ten from "../../images/MenoushPics/10.JPG";
-import eleven from "../../images/MenoushPics/11.JPG";
-import twelve from "../../images/MenoushPics/12.JPG";
-import thirteen from "../../images/MenoushPics/13.JPG";
-import fourteen from "../../images/MenoushPics/14.JPG";
-import fifteen from "../../images/MenoushPics/15.JPG";
 
 function UserCarousal() {
 
